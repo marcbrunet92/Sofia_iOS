56103dd867cd051dd1443e52f25282e45e19d80a
Marc Brunet <marc@lemarc.fr>
2026-05-23 10:37:58 +0200

    Initial Commit

---
+++ Sofia/SofiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofiaApp.swift
+//  Sofia
+//
+//  Created by Marc Brunet on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SofiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
