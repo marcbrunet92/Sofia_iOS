e3af4f1efc728e9ba0a9124531406dda3e9b0ce3
Marc Brunet <marc@lemarc.fr>
2026-05-26 20:21:48 +0200

    add empty widget

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Marc Brunet on 26/05/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SofiaWindgetBundle: WidgetBundle {
+    var body: some Widget {
+        SofiaWidget()
+    }
+}
